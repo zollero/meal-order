4b2a544002cbef7a9785baaaeb7d1c47e2f867e5
Henry <corona7@163.com>
2016-08-06 00:51:28 +0800

    add files

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,13 @@
 
 
 angular.module('mealOrder', [])
-.controller('MenuController', function () {
-
+.controller('MenuController', function ($scope) {
+    
+    $scope.menus = [{
+        name: '麻婆豆腐',
+        price: 12
+    }, {
+        name: '土豆烧鸡',
+        price: 20
+    }]
 });
