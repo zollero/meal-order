d688bf0731fe6256bb2d0add8b0286c9060e20d0
Henry <corona7@163.com>
2016-08-10 10:29:56 +0800

    fix

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,7 +5,7 @@ angular.module('mealOrder', [])
 
     $http({
         method: 'get',
-        url: 'http://localhost:3000/menu/all'
+        url: 'http://10.224.195.144:3000/menu/all'
     }).success(function (data, status) {
         console.log(data);
         $scope.menus = data.menus;
@@ -16,47 +16,70 @@ angular.module('mealOrder', [])
     $scope.selectedMeals = [];
 
     //创建一个socket连接
-    var socket = io('ws://localhost:3000/');
+    var socket = io('ws://10.224.195.144:3000/');
 
     socket.on('news', function (data) {
         console.log(data);
     });
 
     //监听其他人选菜的事件
-    socket.on('someOneAddMeal', function (data) {
-        var index = $scope.selectedMeals.indexOf(meal);
-        if (index === -1) {
-            $scope.menus[$scope.menus.indexOf(meal)].selected = true;
-            $scope.selectedMeals.push(meal);
+    socket.on('someOneAddMeal', function (meal) {
+        console.log(meal);
+        var index = checkMealIndex(meal);
+        if (index.selectedMenuIndex === -1) {
+            $scope.menus[index.menuIndex].selected = true;
+            $scope.selectedMeals.push({
+                name: $scope.menus[index.menuIndex].name,
+                price: $scope.menus[index.menuIndex].price
+            });
         }
     });
     //监听其他人删除选菜的事件
-    socket.on('someOneDeleteMeal', function (data) {
-        var index = $scope.selectedMeals.indexOf(meal);
-        if (index !== -1) {
-            $scope.menus[$scope.menus.indexOf(meal)].selected = false;
-            $scope.selectedMeals.splice(index, 1);
+    socket.on('someOneDeleteMeal', function (meal) {
+        console.log(meal);
+        var index = checkMealIndex(meal);
+        if (index.selectedMenuIndex !== -1) {
+            $scope.menus[index.menuIndex].selected = false;
+            $scope.selectedMeals.splice(index.selectedMenuIndex, 1);
         }
     });
 
-
     //选菜
     $scope.addMeal = function (meal) {
-        var index = $scope.selectedMeals.indexOf(meal);
-        if (index === -1) {
-            $scope.menus[$scope.menus.indexOf(meal)].selected = true;
-            $scope.selectedMeals.push(meal);
+        var index = checkMealIndex(meal);
+        if (index.selectedMenuIndex === -1) {
+            $scope.menus[index.menuIndex].selected = true;
+            $scope.selectedMeals.push({
+                name: $scope.menus[index.menuIndex].name,
+                price: $scope.menus[index.menuIndex].price
+            });
             socket.emit('addMeal', meal);
         }
     };
 
     //取消选菜
     $scope.deleteMeal = function (meal) {
-        var index = $scope.selectedMeals.indexOf(meal);
-        if (index !== -1) {
-            $scope.menus[$scope.menus.indexOf(meal)].selected = false;
-            $scope.selectedMeals.splice(index, 1);
+        var index = checkMealIndex(meal);
+        if (index.selectedMenuIndex !== -1) {
+            $scope.menus[index.menuIndex].selected = false;
+            $scope.selectedMeals.splice(index.selectedMenuIndex, 1);
             socket.emit('deleteMeal', meal);
         }
     };
+
+    function checkMealIndex(meal) {
+        var indexObj = {};
+        angular.forEach($scope.menus, function (value, index) {
+            if (value.name === meal.name) {
+                indexObj.menuIndex = index;
+            }
+        });
+        angular.forEach($scope.selectedMeals, function (value, index) {
+            if (value.name === meal.name) {
+                indexObj.selectedMenuIndex = index;
+            }
+        });
+        if (!indexObj.selectedMenuIndex) indexObj.selectedMenuIndex = -1;
+        return indexObj;
+    }
 });
