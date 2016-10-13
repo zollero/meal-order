9b2e4fb944ea4eb78a3f992052528dfbe2c3d75b
flyfish <corona7@163.com>
2016-10-14 00:20:38 +0800

    添加菜单的操作按钮

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -2,5 +2,12 @@
 
 ;(function (window, document, $) {
 
+    $('.edit-btn').on('click', function () {
+        //TODO 考虑怎么将参数传递给后端，后端需要对这些参数做怎样的处理
+    });
+
+    $('.del-btn').on('click', function () {
+        //TODO 给一个确认提示
+    });
 
 }(window, document, $));
