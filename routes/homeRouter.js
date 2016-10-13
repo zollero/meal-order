be75745800363b70a889ee96d5d372b46375fc6a
cor7ona <corona7@163.com>
2016-10-13 13:17:42 +0800

    '优化'

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,12 +1,10 @@
 
-
 'use strict';
 
 var express = require('express');
 var router = express.Router();
 var db = require('../database/model');
 var util = require('./routerUtil');
-
 
 router.get('/home', function (req, res) {
     if (!util.authentication(req, res)) return;
