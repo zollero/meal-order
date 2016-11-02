deea7c95fba59517107569888da79431f30009bb
cor7ona <corona7@163.com>
2016-11-02 14:05:27 +0800

    完善点餐的动画及多种客户端的样式

--- public/javascripts/meal.js
+++ public/javascripts/meal.js
@@ -8,9 +8,33 @@
     var dishListEle = $('#dish-list'),
         addDishBtn = $('.add-dish-btn'),
         basketInfo = $('#basket-info'),
-        cartBasket = $('#cart-basket')[0];
+        cartBasket = $('#cart-basket')[0],
+        submitBtn = $('#submit-meal-btn');
 
-    var addMealURL = 'ws://' + document.location.host;
+    //弹出提示的样式类
+    var messageObjs = {
+        success: {
+            className: 'dis-message-success',
+            iconClassName: 'glyphicon-ok-sign'
+        },
+        info: {
+            className: 'dis-message-info',
+            iconClassName: 'glyphicon-info-sign'
+        },
+        warning: {
+            className: 'dis-message-warning',
+            iconClassName: 'glyphicon-exclamation-sign'
+        },
+        danger: {
+            className: 'dis-message-danger',
+            iconClassName: 'glyphicon-minus-sign'
+        }
+    };
+
+    var urlSearch = window.location.search;
+    //var urlParams =
+
+    var addMealURL = '/ordering/';
 
     /**
      * TODO
@@ -43,9 +67,16 @@
         var cartBasketTop = cartBasket.style.top;
         if (cartBasketTop === '0px' || cartBasketTop === '') {
             cartBasket.style.top = cartBasketHeight + 'px';
+            cartBasket.style.height = '0px';
         } else {
             cartBasket.style.top = '0px';
+            cartBasket.style.height = 'auto';
         }
+    });
+
+    submitBtn.on('click', function(e) {
+        e.preventDefault();
+        console.log(111);
     });
 
     addDishBtn.on('click', function() {
@@ -62,7 +93,6 @@
                 '<input type="text" readonly class="selected-no" value="1" />' +
                 '<button type="button" class="plus-btn">+</button></div>' +
                 '<div class="total-price">¥' + dishPrice + '</div></div>';
-
             thisDishLine = dishListEle.append(basketLine).find('.dish-line[dish-id=' + dishId + ']');
             //给增加和减少按钮添加事件处理函数
             initMPEvent(thisDishLine);
@@ -74,6 +104,8 @@
             selectedNoEle.val(selectedNo);
             thisTotalPriceEle.text('¥' + selectedNo * Number(dishPrice));
         }
+        refreshTotalDishesInfo();
+        showMessage('info', '添加了一个菜');
         //TODO  修改左下角购物车上的数字
         //TODO emit一个事件给当前点餐的其他人
     });
@@ -91,18 +123,62 @@
                 target.remove();
                 resizeDishListHeight();
             }
+            refreshTotalDishesInfo();
             //TODO emit事件给其他人，减少了一个菜
         });
         target.find('.plus-btn').on('click', function() {
             var value = Number(selectedNoEle.val()) + 1;
             selectedNoEle.val(value);
             totalPriceEle.text('¥' + value * dishPrice);
+            refreshTotalDishesInfo();
             //TODO emit事件给其他人，添加了一个菜
         });
     }
 
     function resizeDishListHeight() {
         dishListEle[0].style.height = dishListEle.find('.dish-line').length * DISH_LINE_HEIGHT + 'px';
+    }
+
+    function refreshTotalDishesInfo() {
+        var dishLines = dishListEle.find('.dish-line').toArray();
+        var totalNo = 0;
+        var totalPrice = 0;
+        $.each(dishLines, function(i, v) {
+            var dishNo = Number($(v).find('.selected-no').val());
+            var price = Number($(v).attr('dish-price'));
+            totalNo += dishNo;
+            totalPrice += dishNo * price;
+        });
+        totalNo = totalNo > 99 ? '99+' : totalNo;
+        basketInfo.find('.badge').text(totalNo);
+        basketInfo.find('.total-price').text(totalPrice);
+    }
+
+    function showMessage(type, message) {
+        var messageContainer = document.querySelector('.dis-message-container');
+        var messageEle,
+            messageObj = messageObjs[type] ? messageObjs[type] : messageObjs['info'],
+            thisMessageEle;
+
+        messageEle = '<li class="dis-message ' + messageObj.className + '">' +
+            '<span class="dis-message-status glyphicon ' + messageObj.iconClassName + '"></span>' +
+            '<div class="essage-text">' + message + '</div> <a class="dis-message-close">' +
+            '<span class="glyphicon glyphicon-remove-sign"></span></a></li>';
+
+        if (!messageContainer) {
+            messageEle = '<ul class="dis-message-container">' + messageEle + '</ul>';
+            $('body').append(messageEle);
+        } else {
+            $('.dis-message-container').append(messageEle);
+        }
+        thisMessageEle = $('.dis-message-container').children().last();
+        thisMessageEle.find('.dis-message-close').on('click', function(e) {
+            var targetMessageEle = e.target.parentElement.parentElement;
+            targetMessageEle.parentElement.removeChild(targetMessageEle);
+        });
+        setTimeout(function() {
+            thisMessageEle.remove();
+        }, 5000);
     }
 
 }(window, document, $));
